232f53dd6c9ea8a21990bae3b0ecbf6730cc253c
Lugo Salazar Brandon Oziel <221G0304@rcarbonifera.tecnm.mx>
2026-03-10 00:46:09 -0600

    Avance #1

---
+++ AdivinaQuienServidor/Models/Comandos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdivinaQuienServidor.Models
+{
+    public enum Orden { Conectar, Esperar, SeleccionarPersonaje, EsperarRespuesta, EsperarPregunta, TerminarPartida }
+    public class Comandos
+    {
+        public Orden Comamando { get; set; }
+    }
+    public class ConectarCommando : Comandos
+    {
+        public string Nombre { get; set; } = null!;
+        public string Personaje { get; set; } = null!;
+    }
+    public class SeleccionarPersonajeCommando : Comandos
+    {
+        public string NombrePersonaje { get; set; } = null!;
+    }
+    public class PreguntaCommando : Comandos
+    {
+        public string Pregunta { get; set; } = null!;
+    }
+    public class RespuestaCommando : Comandos
+    {
+        public bool Respuesta { get; set; }
+    }
+    public class TerminarPartidaCommando : Comandos
+    {
+        public string NombreGanador { get; set; } = null!;
+        public string PersonajeJ1 { get; set; } = null!;
+        public string PersonajeJ2 { get; set; } = null!;
+
+    }
+
+}
---
+++ AdivinaQuienServidor/Services/ServidorService.cs
@@ -0,0 +1,63 @@
+﻿using AdivinaQuienServidor.Models;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+
+namespace AdivinaQuienServidor.Services
+{
+    public class ServidorService
+    {
+        private TcpListener? Servidor { get; set; }
+        public List<Personaje> Personajes { get; set; } = new List<Personaje>();
+        public List<string> HistoriaPyR { get; set; } = new List<string>();
+
+        public string Personaje1 { get; set; } = null!;
+        public string Personaje2 { get; set; } = null!;
+        public string NickPersonaje1 { get; set; } = null!; //Nombre del servidor que pordefecto tendra el turno y personaje #1
+        public string NickPersonaje2 { get; set; } = null!;
+
+        int puerto = 5000;
+        bool juegoIniciado = false;
+        public string Pregunta { get; set; } = null!;
+        public string Respuesta { get; set; } = null!;
+
+        public void AbrirSala()
+        {
+            if (juegoIniciado == false)
+            {
+                juegoIniciado = true;               
+                Thread Hilo = new Thread(RecibirJugador2); //Hilo para recibir al jugador 2 y comenzar el juego
+                Hilo.IsBackground = true;
+                Hilo.Start();
+            }
+        }
+
+        private void RecibirJugador2(object? obj)
+        {
+            IPEndPoint Ipserver = new(IPAddress.Any, puerto);
+            Servidor = new(Ipserver);
+            try
+            {
+                var clieneNuevo = Servidor.AcceptTcpClient();
+                var stream = clieneNuevo.GetStream();
+
+                byte[] buffer = new byte[clieneNuevo.Available];
+                var json = Encoding.UTF8.GetString(buffer);
+                var ConectarCommand = JsonSerializer.Deserialize<ConectarCommando>(json);
+                if (ConectarCommand != null)
+                {
+                    Personaje2 = ConectarCommand.Personaje;
+                    NickPersonaje2 = ConectarCommand.Nombre;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
